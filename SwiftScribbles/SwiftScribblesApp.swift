cf332433f7781a82edea276a6a8bd0287f13324e
ferlarag <89590031+ferlarag@users.noreply.github.com>
2024-03-08 09:33:42 -0600

    Initial Commit

---
+++ SwiftScribbles/SwiftScribblesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftScribblesApp.swift
+//  SwiftScribbles
+//
+//  Created by Neftalí Lara on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftScribblesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
