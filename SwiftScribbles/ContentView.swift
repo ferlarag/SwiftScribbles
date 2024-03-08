a9f82e3e19ff6ff3cdfcd16536b8e078818dfe14
ferlarag <89590031+ferlarag@users.noreply.github.com>
2024-03-08 11:31:39 -0600

    Initial commit

--- SwiftScribbles/ContentView.swift
+++ SwiftScribbles/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationSplitView {
+            List {
+                Section{
+                    NavigationLink {
+                        Text("Item 2")
+                    } label: {
+                        HStack(spacing: 12){
+                            Circle()
+                                .frame(height: 32)
+                                .foregroundStyle(Gradient(colors: [Color.blue, Color.cyan]))
+                            VStack(alignment: .leading){
+                                Text("Welcome")
+                                    .font(.title2)
+                                    .fontWeight(.semibold)
+                                Text("Learn how to learn from this app")
+                                    .fontWeight(.light)
+                                    .foregroundStyle(Color.gray)
+                            }
+                        }
+                    }
+                }
+                Section("Upcoming projects"){
+                    NavigationLink {
+                        Text("A Game project")
+                    } label: {
+                        Label("2D Game", systemImage: "gamecontroller")
+                    }
+                    NavigationLink {
+                        Text("AI Chat")
+                    } label: {
+                        Label("AI Chat", systemImage: "message.badge.waveform")
+                    }
+                }
+            }.navigationTitle("Swift Scribbles")
+        } detail: {
+            Text("Something here")
         }
-        .padding()
+
     }
 }
 
